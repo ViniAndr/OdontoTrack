059a9ed5ff67483dab25edfcef9d755a16a65f04
Fabio Vinicius <vinicius@Mac-mini-de-Fabio.local>
2026-03-26 18:33:42 -0300

    estrutura inicial Spring Boot e documentação

---
+++ src/main/java/com/odontotrack/api/OdontotrackApplication.java
@@ -0,0 +1,13 @@
+package com.odontotrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontotrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odontotrack/api/OdontotrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontotrack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontotrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
