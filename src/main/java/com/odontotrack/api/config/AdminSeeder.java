42aed4943fe31f082685d347569d757c4a2228ce
ViniAndr <fabioviniciusbarbosa@hotmail.com>
2026-04-05 20:29:57 -0300

    Adição da dependencia do H2 e criação de um semeador automatico para o usuario admin

---
+++ src/main/java/com/odontotrack/api/config/AdminSeeder.java
@@ -0,0 +1,36 @@
+package com.odontotrack.api.config;
+
+import com.odontotrack.api.model.Perfil;
+import com.odontotrack.api.model.Profissional;
+import com.odontotrack.api.repository.ProfissionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSeeder implements CommandLineRunner {
+
+    @Autowired
+    private ProfissionalRepository profissionalRepository;
+
+    @Override
+    public void run(String... arg) throws Exception{
+        // Verifica se o banco já possui algum registro. Se estiver vazio, cria o Admin.
+        if (profissionalRepository.count() == 0) {
+            Profissional admin = new Profissional();
+            admin.setNome("Administrador do Sistema");
+            admin.setCpf("00000000000"); // 11 dígitos
+            admin.setTelefone("00000000000");
+            admin.setEmail("admin@odontotrack.com");
+            admin.setSenha("123456"); // No futuro, usaremos BCrypt aqui para criptografar
+
+            // Adiciona o perfil de Administrador
+            admin.getPerfis().add(Perfil.ROLE_ADMIN);
+
+            profissionalRepository.save(admin);
+            System.out.println("✅ Usuário Admin padrão criado com sucesso!");
+        } else {
+            System.out.println("⚡ Banco de dados já populado. Nenhuma ação necessária no Seeder.");
+        }
+    }
+}
